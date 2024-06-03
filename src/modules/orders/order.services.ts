ba72e9508a9597f2d9ee2735565446bbae098e4c
ibrahim <ibrahimrabbihere@gmail.com>
2024-06-04 00:44:05 +0600

    inventory management logic implement done

--- src/modules/orders/order.services.ts
+++ src/modules/orders/order.services.ts
@@ -1,10 +1,23 @@
+import { productModel } from "../myGedget/model";
 import { OrederInterface } from "./order.intrerface";
 import { orderModel } from "./order.model";
 
 
 export const insertOrderService = async (data: OrederInterface) => {
     const inserting = await orderModel.create(data)
-    return inserting
+
+    if (inserting.productId) {   
+        const id = { _id: data.productId }
+        const findData = await productModel.findById(id)
+        const totalQunty: unknown = findData?.inventory.quantity
+        if (typeof totalQunty === 'number') {
+            const change = { $set: { 'inventory.quantity': totalQunty - data.quantity } }
+            const update = await productModel.updateOne(id, change)
+            console.log(update)
+            return inserting
+        }
+       
+    }
 }
 
 
